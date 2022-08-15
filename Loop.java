ab7da16aa7bd621f1ef1c0f11fcd5c4ed4d4179c
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-15 19:09:29 -0400

    add: aula 31

---
+++ Loop.java
@@ -0,0 +1,26 @@
+public class Loop {
+  public static void main(String[] args) {
+    
+    // while
+    int x=0;
+    while (x < 10) {
+      System.out.print(x + "\n");
+      x++;
+    }
+    System.out.print("\n");
+    
+    // for
+    for (int i = 0; i < 10; i++) {
+      System.out.print(i + "\n");
+    }
+    int y = 0;
+    
+    // do while
+    do {
+      System.out.print(y + "\n");
+      y++;
+    }while (y < 10);
+    System.out.print("\n");
+    
+  }
+}
