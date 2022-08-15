700a1a235e449a2f8fcf37963177a9bcb2ddb307
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-08-15 02:38:20 -0400

    add: aula 21

---
+++ Condicionais.java
@@ -0,0 +1,16 @@
+public class Condicionais {
+  public static void main(String[] args){
+  
+    int x = 5;
+    if(x == 5) {
+      System.out.print("iqual a 5");
+    } 
+    else if(x > 5) {
+      System.out.print("maior que 5");
+    }
+    else{
+      System.out.print("menor que 5");
+    }
+  
+  }
+}
--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,41 @@
-public class Operadores {
-  
-}
+/*
+  +	  Adição        Adiciona dois valores................. x + y	
+  -	  Subtração	    Subtrai um valor de outro............. x - y	
+  *	  Multiplicação	Multiplica dois valores............... x * y	
+  /	  Divisão	      Divide um valor por outro............. x / y	
+  %   Módulo	      Retorna o restante da divisão......... x% y	
+  ++	Incremento    Aumenta o valor de uma variável em 1.. ++ x	
+  -	  Decremento    Diminui o valor de uma variável em 1.. --x
+*/
+
+/*
+  Operadores de atribuição Java
+  =   x = 5	  x= 5
+  +=  x += 3	x= x+3	
+  -=  x -= 3	x= x-3	
+  *=  x *= 3	x= x*3	
+  /=  x /= 3	x= x/3	
+  %=  x %= 3	x= x%3	
+  &=  x &= 3	x= x&3	
+  |=  x |= 3	x= x|3	
+  ^=  x ^= 3	x= x^3	
+  >>= x >>= 3	x= x>3	
+  <<= x <<= 3	x= x<3
+*/
+
+/*
+  Operadores de comparação java
+  ==	Igual a........... x == y	
+  =!	Não é igual....... x != y	
+  >	  Maior que......... x > y	
+  <  	Menos que......... x < y	
+  >=	Maior ou igual a.. x >= y	
+  <=	Menos ou igual a.. x <= y
+*/
+
+/*
+  Operadores lógicos java
+  &&  Lógico e	Retorna true se ambas as afirmações forem verdadeiras... x < 5 &&  x < 10	
+  ||  Lógico ou	Retorna true se uma das afirmações for verdadeira....... x < 5 || x < 4	
+  !   Retorna false se o resultado for verdadeiro....................... !(x < 5 && x < 10)
+*/
